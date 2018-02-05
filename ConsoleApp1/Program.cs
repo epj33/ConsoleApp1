329fadd9b00e353e7db6b5c02fb6179a6d61464f
epj33 <epj33@msn.com>
2018-02-05 11:52:15 -0700

    new stuff. first commit?
    i guess?

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+
+namespace ConsoleApp1 {
+    class Program {
+
+        static void RunUpdate(String cloudVersionString) 
+        {
+            string updateCloudString = "RunUpdate is updating to version " + cloudVersionString + "...";
+            string versionFilePath = @"c:\users\michaelstahle\documents\visual studio 2017\Projects\ConsoleApp1\ConsoleApp1\localVersion.txt";
+            File.WriteAllText( versionFilePath, cloudVersionString );
+            Console.WriteLine( updateCloudString );
+        }//end RunUpdate();
+
+
+        static Version GetCloudVersion() {
+            string versionFilePath = @"c:\users\michaelstahle\documents\visual studio 2017\Projects\ConsoleApp1\ConsoleApp1\cloudVersion.txt";
+            //string versionFilePathe = @"E:\eric\progFiles\jenkins\workspace\versionCheckFromGithub\luvCloudVersion.txt";
+            var versionFromFile = new Version( "0.0.0" );
+            if( File.Exists( versionFilePath ) )
+            {
+                //versionFromFile = new Version( File.ReadAllText( versionFilePath ) );//works. need better in case of more lines?
+                StreamReader versionFile = new StreamReader( versionFilePath );
+                versionFromFile = new Version( versionFile.ReadLine() );
+                versionFile.Close();
+            }
+            return versionFromFile;
+        }//end GetCloudVersion()
+
+
+        static Version GetLocalVersion() 
+        {
+            string versionFilePath = @"c:\users\michaelstahle\documents\visual studio 2017\Projects\ConsoleApp1\ConsoleApp1\localVersion.txt";
+            var versionFromFile = new Version( "0.0.0" );
+            if( File.Exists( versionFilePath ) ) 
+            {
+                StreamReader versionFile = new StreamReader( versionFilePath );
+                versionFromFile = new Version( versionFile.ReadLine() );
+                versionFile.Close();
+            }
+            else {
+                File.WriteAllText( versionFilePath, versionFromFile.ToString() );
+            }
+            return versionFromFile;
+        }//end GetLocalVersion()
+
+
+        static void Main( string[] args ) {
+            Version cloudVersion;
+            if( args.Length == 0 )
+            {
+                cloudVersion = GetCloudVersion();
+            }
+            else {
+                cloudVersion = new Version(args[0]);
+            }
+            Version localVersion = GetLocalVersion();
+            
+            if ( cloudVersion.CompareTo( localVersion ) > 0 )
+            {
+                RunUpdate( cloudVersion.ToString() );
+            }
+            Console.WriteLine( "version is up to date!\nHit Enter to close." );
+            //Console.ReadLine();
+        }//end Main()
+    }//end Program
+}//end ConsoleApp1
